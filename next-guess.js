fc9066b74954eb01d46babaf6cff83deb193be00
Beau <beaulm@gmail.com>
2017-06-19 10:57:12 -0700

    Add documentation to next-guess functions

--- next-guess.js
+++ next-guess.js
@@ -1,13 +1,11 @@
-function nextGuess(gameState, usedLetters, remainingLetters, wordList) {
-
-  //Replace all underscores in the game state with our bracket string
-  let regex = new RegExp('\,('+gameState.replace(/_/g, remainingLetters)+')\,', 'gi');
-
-  //Get all possible words for the current game state
-  let possibleWords = wordList.match(regex);
-
-  //Update our word list to the new (reduced) set of words
-  wordList = possibleWords.join('');
+/**
+	* @desc getLetterFrequencies() Builds an map of letters and their frequencies for a given word list
+	*
+	* @param {array} [possibleWords] - Array of words
+	*
+	* @return {object} - A map of letters and thier frequencies in the given word list
+	*/
+function getLetterFrequencies(possibleWords) {
 
   //Initialize an object to keep track of letter frequencies
   let letterFrequencies = {a: 0, b: 0, c: 0, d: 0, e: 0, f: 0, g: 0, h: 0, i: 0, j: 0, k: 0, l: 0, m: 0, n: 0, o: 0, p: 0, q: 0, r: 0, s: 0, t: 0, u: 0, v: 0, w: 0, x: 0, y: 0, z: 0};
@@ -55,6 +53,21 @@ function nextGuess(gameState, usedLetters, remainingLetters, wordList) {
 
   }
 
+  //Return the letter frequencies
+  return letterFrequencies;
+
+}
+
+/**
+	* @desc getMostCommonLetter() Given a letter frequencies object, find the most common letter not already used
+	*
+	* @param {object} [letterFrequencies] - Map of letter frequencies
+	* @param {string} [usedLetters] - List of already used letters
+	*
+	* @return {string} - The most common, not already guessed letter
+	*/
+function getMostCommonLetter(letterFrequencies, usedLetters) {
+
   //Initialize variables to keep track of the most common letter so far
   let highestCount = 0;
   let highestLetter = '';
@@ -78,6 +91,46 @@ function nextGuess(gameState, usedLetters, remainingLetters, wordList) {
     }
 
   }
+
+  //Return the highest unused letter
+  return highestLetter;
+
+}
+
+/**
+	* @desc nextGuess() Figure out what letter to guess next (and what the resulting list of possible words will be)
+	*
+	* @param {string} [gameState] - String representing the current game state (ex: h__s_)
+	* @param {string} [usedLetters] - List of already used letters
+	* @param {string} [remainingLetters] - List of unused letters in brackets (ex: [kvxzjq])
+	* @param {string} [wordList] - List of words that could be solutions to the current game state, separated by commas (ex: ,ace,,add,,are,,dad,)
+	*
+	* @return {object} - The letter to guess next and the resulting word list
+	*/
+function nextGuess(gameState, usedLetters, remainingLetters, wordList) {
+
+  //Replace all underscores in the game state with our bracket string
+  let regex = new RegExp('\,('+gameState.replace(/_/g, remainingLetters)+')\,', 'gi');
+
+  //Get all possible words for the current game state
+  let possibleWords = wordList.match(regex);
+
+  //If we didn't match any words in our dictionary
+  if(possibleWords.length === 0) {
+
+    //Send back whatever the next most common letter in english is
+    return {letter: remainingLetters.charAt(1);, wordList: wordList};
+
+  }
+
+  //Update our word list to the new (reduced) set of words
+  wordList = possibleWords.join('');
+
+  //Get the letter frequencies for all letters in all the words in our list
+  let letterFrequencies = getLetterFrequencies(possibleWords);
+
+  //Find the most commonly used letter based on our letterFrequencies object
+  let highestLetter = getMostCommonLetter(letterFrequencies, usedLetters);
 
   return {letter: highestLetter, wordList: wordList};
 
