b038a471617872f2b04989b22a08951a52d982cc
Beau <beaulm@gmail.com>
2017-06-18 23:35:27 -0700

    Store bitmap in redis instead of on disk

--- build-bitmap.js
+++ build-bitmap.js
@@ -1,15 +1,9 @@
-const fs = require('fs');
+const redis = require('redis'), client = redis.createClient();
 
 //Cache all possible binary numbers up to 31 bits
 
-//Initialize our bitmap
-let bitmap = [];
-
 //For each bit up to 31
 for(let bits=0; bits<=31; bits++) {
-
-  //Add a new array to our bitmap
-  bitmap.push([]);
 
   //For every possible binary number of the current length
   for(let num=0; num<Math.pow(2,bits)-1; num++) {
@@ -17,17 +11,21 @@ for(let bits=0; bits<=31; bits++) {
     //Get the current binary number
     let binary = num.toString(2);
 
+    //Get the output length for substr
+    let outputLength = bits*-1;
+
     //Pad it with zero's to the appropriate length
-    let padded = ('0000000000000000000000000000000'.substr(binary.length)+binary).substr(bits);
+    let padded = ('0000000000000000000000000000000'.substr(binary.length)+binary).substr(outputLength);
+
+    //Create a key to access our padded number with
+    let key = num+'-'+bits;
 
     //Add it to our bitmap
-    bitmap[bits].push(padded);
+    client.set(key, padded, redis.print);
 
   }
 
 }
 
-fs.writeFile('bitmap.js', JSON.stringify(bitmap), (err) => {
-  if (err) throw err;
-  console.log('Bitmap created!');
-});
+client.quit();
+process.exit();
